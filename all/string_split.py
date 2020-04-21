367ba520ae8c933d297d777838ce7dcb12f4cebd
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-04-22 00:21:46 +0530

    string splitting
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ all/string_split.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+Purpose: Splitting a string without using str.split()
+"""
+
+
+def split_string(word):
+    result = []
+    temp_val = ''
+    for each_char in word:
+        if each_char == ':':
+            result.append(temp_val)
+            temp_val = ''
+        else:
+            temp_val += each_char
+    else:
+        result.append(temp_val)
+    return result
+
+if __name__ == '__main__':
+    assert split_string('a:b:c:324324:45') == ['a', 'b', 'c', '324324', '45']
+    assert split_string('a:b:c::324324:45') == ['a', 'b', 'c', '', '324324', '45']
