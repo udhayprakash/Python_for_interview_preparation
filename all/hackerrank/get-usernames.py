cd062bb267152c862abd5e0b46950b21d116c8e1
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-02-02 04:02:19 +0530

    updated

---
+++ all/hackerrank/get-usernames.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getUsernames' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts INTEGER threshold as parameter.
+#
+# URL for cut and paste
+# https://jsonmock.hackerrank.com/api/article_users?page=<pageNumber>
+import urllib.request
+import json
+
+
+def getUsernames(threshold):
+    result = []
+    pageNumber = 1
+    while True:
+        URL = f'https://jsonmock.hackerrank.com/api/article_users?page={pageNumber}'
+        response = urllib.request.urlopen(URL)
+        response_data = json.loads(response.read())
+        for eachrecord in response_data['data']:
+            if eachrecord['submission_count'] > threshold:
+                result.append(eachrecord['username'])
+        pageNumber += 1
+        if response_data['page'] == response_data['total_pages']:
+            break
+        print(eachrecord)
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    threshold = int(input().strip())
+
+    result = getUsernames(threshold)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ all/hackerrank/get_equal-half_sums.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Purpose: In the given list of numbers,
+    return two lists of equal sums
+"""
+
+
+def myfunc(arr):
+    half = sum(arr)/2
+    part_sum = 0
+    for index, num in enumerate(arr):
+        part_sum += num
+        if part_sum >= half:
+            break
+    else:
+        return -1
+    return [arr[:index+1], arr[index+1:]]
+
+
+print(myfunc([1, 1, 1, 2, 1]))
+assert myfunc([1, 1, 1, 2, 1]) == [[1, 1, 1], [2, 1]]
+assert myfunc([2, 2]) == [[2], [2]]
+assert myfunc([1, 2, 3, 4, 6, 4]) == [[1, 2, 3, 4], [6, 4]]
