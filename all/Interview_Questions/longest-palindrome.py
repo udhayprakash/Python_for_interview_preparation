97593279d5236f5b2b6599a8b76d4f6f52948031
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-02-15 04:14:51 +0530

    updated

---
+++ all/Interview_Questions/longest-palindrome.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Problem Name: Find Longest Palindromic Substring 
+
+Problem Statement: Given a string s, find and return the longest
+    palindromic substring in s. 
+    You may assume that the maximum length of s is 1000.
+
+    * Example(s): the longest palindrome in the string "ababd" is "aba" or "bab" (either is acceptable) and we would want either returned 
+    * Edge Cases: what happens when the string is of length 1 or 0?
+    * Inputs: asdfsda, hellollehasdf, abahellollehesdf, abacdcf */regsdgsdgdsgsggsdgdsgsdgsgsgsfasfafafaf
+"""
+
+
+def getSubString(s, l, r):
+    while l >= 0 and r < len(s) and s[r] == s[l]:
+        l -= 1
+        r += 1
+
+    return s[l+1:r]
+
+
+def longestPalindrome(s):
+    largest = ""
+    for i in range(len(s)):
+        for k in range(2):
+            temp = getSubString(s, i, i+k)
+            if len(temp) > len(largest):
+                largest = temp
+    return largest
+
+
+print(longestPalindrome('ababd'))
