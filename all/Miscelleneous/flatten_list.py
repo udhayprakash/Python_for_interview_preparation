be38aad6b196fb5a0cc4f768b8c1915c7771ca4e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-02-25 02:26:36 +0530

    updated

--- all/Miscelleneous/flatten_list.py
+++ all/Miscelleneous/flatten_list.py
@@ -4,14 +4,25 @@ Purpose: List flattening
 """
 
 
-def flatten_list(given_list, final_list=[]):
+def flatten(given_list, final_list=[]):
     for each_ele in given_list:
         if isinstance(each_ele, list):
-            flatten_list(each_ele, final_list)
+            flatten(each_ele, final_list)
         else:
             final_list += [each_ele]
     return final_list
 
 
+def flatten2(given_list):
+    final_list = []
+    for each_ele in given_list:
+        if isinstance(each_ele, list):
+            final_list.extend(flatten2(each_ele))
+        else:
+            final_list.append(each_ele)
+    return final_list
+
+
 if __name__ == '__main__':
-    assert flatten_list([1, [2, 3], 4, [5, [6, [7]]]]) == [1, 2, 3, 4, 5, 6, 7]
+    assert flatten([1, [2, 3], 4, [5, [6, [7]]]]) == [1, 2, 3, 4, 5, 6, 7]
+    assert flatten2([1, [2, 3], 4, [5, [6, [7]]]]) == [1, 2, 3, 4, 5, 6, 7]
