6190d4ab78113ce9a96b719b24c78d4b95dd612b
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-03-04 18:37:57 +0530

    grouping anagrams

---
+++ all/Miscelleneous/anagrams_list.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+def group_anagrams(words):
+    result = defaultdict(list)
+    for word in words:
+        result[str(sorted(word))].append(word)
+    return list(result.values())
+
+
+print(group_anagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+
+
+def group_anagrams(words):
+    result = {}
+    for word in words:
+        sorted_word_str = str(sorted(word))
+        result.setdefault(sorted_word_str, [])
+        result[sorted_word_str].append(word)
+    return list(result.values())
+
+
+print(group_anagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
