3d3db40e30b1946ba4fae2fea74898ac70353a6d
egoae <udhayprakash.pethakamsetty.ext@bayer.com>
2022-05-27 13:52:29 +0530

    max profit

--- all/Miscelleneous/max_profit.py
+++ all/Miscelleneous/max_profit.py
@@ -1,18 +1,39 @@
 def solution(prices):
-    if not prices:
-        return 0
-    elif len(prices) == 1:
-        return prices[0]
-    max_profit = 0
-    for index, price in enumerate(prices):
-        relative_gain = price - prices[index - 1]
-        if relative_gain > max_profit:
-            max_profit = relative_gain
-    return max_profit
+    m = 0
+    for i in range(0, len(prices)):
+        for j in range (i + 1, len(prices)):
+            m = max(m, prices[j] - prices[i])
+    return m
+
+def solution(prices):
+    buy, m = 0, 0
+    for i in range(len(prices)):
+        buy = min(buy, prices[i])
+        profit = prices[i] - buy
+        m = max(m, profit)
+    return m
+
+def solution(prices):
+    m = 0
+    for i in range(0, len(prices)-1):
+        buy, sell = prices[i], max(prices[i+1:])
+        m = max(m, sell-buy)
+    return m
+
+
+def solution(prices):
+      max_profit = 0
+      min_stock = float('inf')
+      for price in prices:
+         max_profit = max(max_profit, price - min_stock)
+         min_stock = min(min_stock, price)
+      return max_profit
 
 
 assert solution([6, 0, -1, 10]) == 11
 assert solution([13, 10, 8, 4, 10]) == 6
+assert solution([10, 12, 9, 6, 8, 12]) == 6
+assert solution([455, 460, 465, 451, 414, 415, 441]) == 27
 print(
     solution(
         [
