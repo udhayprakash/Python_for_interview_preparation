82c55bfec903bd9e01ff0d3cbc7f99c0f675e094
egoae <udhayprakash.pethakamsetty.ext@bayer.com>
2022-05-31 22:12:13 +0530

    palindrome substrs

---
+++ all/Interview_Questions/get_palindrome_substrs.py
@@ -0,0 +1,17 @@
+import enum
+
+
+def get_palindrome_substrs(givenStr):
+    unique_palindrome_substrs = set()
+    if givenStr == givenStr[::-1]: unique_palindrome_substrs.add(givenStr)
+    for i, _ in enumerate(givenStr):
+        j = 0
+        while j < len(givenStr):
+            substr = givenStr[j : j+i]
+            if substr == substr[::-1]: unique_palindrome_substrs.add(substr)
+            j += 1
+
+    return unique_palindrome_substrs
+
+print(get_palindrome_substrs('adada'))
+assert get_palindrome_substrs('adada') == {'', 'ada', 'a', 'd', 'adada', 'dad'}
