9fc4b03d4b702bc180d612fb681684174389f285
udhay Prakash <uday3prakash@gmail.com>
2023-02-04 19:15:10 +0000

    updated

--- Interview_Questions/longest_substr_without_repeating_chars.py
+++ Interview_Questions/longest_substr_without_repeating_chars.py
@@ -3,10 +3,20 @@
 Problem: Longest substr Without Repeating Characters
 
 For example:
-"abccba" -> 3 since "abc" or "cba" are substrs without repeating characters
-"abcd" -> 4 since no characters repeat
-"abcdefgha" -> 8 since only the last character repeats
-"abcadef" -> ?
+    Input: s = "abcadbb"
+    Output: 3 Explanation: The answer is "abc", with the length of 3.
+
+    Input: s = "bbbbb"
+    Output: 1 Explanation: The answer is "b", with the length of 1.
+
+    Input: s = "pwwkew"
+    Output: 3 Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+    "abccba" -> 3 since "abc" or "cba" are substrs without repeating characters
+    "abcd" -> 4 since no characters repeat
+    "abcdefgha" -> 8 since only the last character repeats
+    "abcadef" -> ?
 """
 
 
@@ -27,7 +37,31 @@ def longest_substring(somestring: str) -> int:
     return largestSubStrLen
 
 
-assert longest_substring("abcadef") == 4  # 'bcadef'
-assert longest_substring("abccba") == 3  # 'abc'
-assert longest_substring("abcd") == 4  # 'abcd'
-assert longest_substring("abcdefgha") == 8  # 'abcdefgh'
+def longest_substring(somestring: str) -> int:
+    largestSubStrLen = 0
+    substr_chars = {}
+    for ch in somestring:  # O(n)
+        if ch in substr_chars:
+            substr = "".join(substr_chars.keys())
+            if len(substr) > largestSubStrLen:
+                largestSubStrLen = len(substr)
+            substr_chars.clear()
+        substr_chars[ch] = 1
+    else:
+        if substr_chars:
+            substr = "".join(substr_chars.keys())
+            if len(substr) > largestSubStrLen:
+                largestSubStrLen = len(substr)
+    return largestSubStrLen
+
+
+if __name__ == "__main__":
+    assert longest_substring("abcadbb") == 3  # "abc"
+    assert longest_substring("abcacdbb") == 4  # "acdb"
+    assert longest_substring("bbbbb") == 1  # "b"
+    assert longest_substring("pwwkew") == 3  # "wke"
+
+    assert longest_substring("abcadef") == 4  # 'bcadef'
+    assert longest_substring("abccba") == 3  # 'abc'
+    assert longest_substring("abcd") == 4  # 'abcd'
+    assert longest_substring("abcdefgha") == 8  # 'abcdefgh'
